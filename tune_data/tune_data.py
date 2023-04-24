35daf97816a263406582b0c08823c2dc4c005dc9
claytoncook12 <claytoncook12@gmail.com>
2023-04-24 09:07:30 -0400

    update display of tunes data

--- tune_data/tune_data.py
+++ tune_data/tune_data.py
@@ -4,7 +4,7 @@ tune_list: list[Tune] = [
     Tune(
         'Concertina Reel',
         'reel',
-        """T: Concertina Reel,
+        """T: Concertina Reel
 R:reel
 M:C|
 K: D
@@ -13,43 +13,43 @@ K: D
 |:Add2 Add2|AddA BAFA|B2cA BAcA|BAcA BAFG |
   Add2 Add2|AddA BAFE|FABc d2dB|AFEF D4  :|""",
     ['https://www.youtube.com/watch?v=v0EMMbo9ikw']
-    ),
-#--------------------------------------------------------------    
-    Tune('Ships are Sailing',
-         'reel',
-        """T:Ships are Sailing
-R:reel
-M:C|
-K:Edor
-Beed B2dB|AF~F2 dFAF|EFGA BAFB|ABdf eB~B2|
-~e3d B2dB|AF~F2 dFAF|EFGA Bdef|1 gefd Beed:|2 gefd Beef||
-|:~g3a bgeg|fefg afdf|~g3a bgeg|(3fed ed Beef|
-~g3a bgeg|fefg afdf|g2bg f2af|1 gefd Beef:|2 gefd Beed||""",
-    ['https://www.youtube.com/watch?v=9gLsQg38m5E']
-    ),
-#--------------------------------------------------------------
-    Tune('Father Kelly`s',
-        'reel',
-        """T:Father Kelly`s
-R:reel
-M:C|
-K:G
-B2GB AGEG|DGGF G2AB|c2AB cBAG|EAAG FDGA|
-B2GB AGEG|DGGF GABc|d2Bd gdBd|1 cAFA ~G3A:|2 cAFA GABc||
-|:d2Bd gdBd|d2Bd gdBd|e2ce agfe|defg agfe|
-d2Bd gdBd|d2Bd gdBd|c2Ac BAGB|1 AGFA GABc:|2 AGFA ~G3A||)""",
-    ['']
-    ),
-#--------------------------------------------------------------
-    Tune('Thrush in the Straw',
-        'jig',
-        """T:Thrush in the Straw
-R:jig
-M:6/8
-efg edB|cBA BGE|~G3 BdB|BAG ABd|
-efg edB|cBA BGE|~G3 BdB|1 AGF GBd:|2 AGF G2D||
-GBd ~g3|agf g2e|fed edB|AGA BGE|
-GBd ~g3|agf g2e|fed edB|1 AGF G2D:|2 AGF GBd||""",
-    ['']
     )
+#--------------------------------------------------------------    
+#     Tune('Ships are Sailing',
+#          'reel',
+#         """T:Ships are Sailing
+# R:reel
+# M:C|
+# K:Edor
+# Beed B2dB|AF~F2 dFAF|EFGA BAFB|ABdf eB~B2|
+# ~e3d B2dB|AF~F2 dFAF|EFGA Bdef|1 gefd Beed:|2 gefd Beef||
+# |:~g3a bgeg|fefg afdf|~g3a bgeg|(3fed ed Beef|
+# ~g3a bgeg|fefg afdf|g2bg f2af|1 gefd Beef:|2 gefd Beed||""",
+#     ['https://www.youtube.com/watch?v=9gLsQg38m5E']
+#     ),
+#--------------------------------------------------------------
+#     Tune('Father Kelly`s',
+#         'reel',
+#         """T:Father Kelly`s
+# R:reel
+# M:C|
+# K:G
+# B2GB AGEG|DGGF G2AB|c2AB cBAG|EAAG FDGA|
+# B2GB AGEG|DGGF GABc|d2Bd gdBd|1 cAFA ~G3A:|2 cAFA GABc||
+# |:d2Bd gdBd|d2Bd gdBd|e2ce agfe|defg agfe|
+# d2Bd gdBd|d2Bd gdBd|c2Ac BAGB|1 AGFA GABc:|2 AGFA ~G3A||)""",
+#     ['']
+#     ),
+#--------------------------------------------------------------
+#     Tune('Thrush in the Straw',
+#         'jig',
+#         """T:Thrush in the Straw
+# R:jig
+# M:6/8
+# efg edB|cBA BGE|~G3 BdB|BAG ABd|
+# efg edB|cBA BGE|~G3 BdB|1 AGF GBd:|2 AGF G2D||
+# GBd ~g3|agf g2e|fed edB|AGA BGE|
+# GBd ~g3|agf g2e|fed edB|1 AGF G2D:|2 AGF GBd||""",
+#     ['']
+#     )
 ]
