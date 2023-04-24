dcbda9310ad42ed98a0383a8092868470bc90fa3
claytoncook12 <claytoncook12@gmail.com>
2023-04-24 12:41:57 -0400

    tunes update and layout

--- tune_data/model/tune.py
+++ tune_data/model/tune.py
@@ -6,6 +6,57 @@ tune_types: list[str] = [
     'reel','jig','hornpipe','slip jig'
 ]
 
+class YoutubeVideoEmbed:
+    """
+    A class to represent a Embeded Youtube Video
+
+    Attributes
+    ----------
+    title:
+        The Video Title To Display
+    instruments:
+        Instruments playing tune in video
+    embed url:
+        Embed Youtube Url
+    """
+
+    def __init__(
+        self,
+        title: str,
+        instruments: str,
+        embed_url: str
+    ):
+        self.title = title
+        self.instruments = instruments
+        if is_valid_youtube_embed_url(embed_url):
+            self.embed_url = embed_url
+        else:
+            raise ValueError(f'"{embed_url}" is not a valid embed youtube url')
+
+class AudioReferences:
+    """
+    A class that represent a audio file
+
+    Attributes
+    ----------
+    title
+        The Audio Title
+    instruments:
+        Instruments playing tune in audio
+    audio file name
+        The name of the audio file that is located in static/audio
+    """
+
+    def __init__(
+        self,
+        title: str,
+        instruments: str,
+        audio_file_name: str,
+    ):
+        self.title = title
+        self.instruments = instruments
+        self.audio_file_name = audio_file_name
+
 class Tune:
     """
     A class used to represent a Tune
@@ -28,7 +79,8 @@ class Tune:
         title: str,
         tune_type: str,
         abc_notation: str,
-        reference_videos: list[str]
+        reference_videos: list[YoutubeVideoEmbed],
+        reference_audios: list[AudioReferences]
     ):
         self.title = title
         if is_valid(tune_type, tune_types):
@@ -37,6 +89,14 @@ class Tune:
             raise ValueError(f'"{tune_type}" is not a valid tune type.\n\tValid tune types are {tune_types}')
         self.abc_notation = abc_notation
         self.reference_videos = reference_videos
+        self.reference_audios = reference_audios
+
 
 def is_valid(input:str, valid_input: list[str]):
     return input in valid_input
+
+def is_valid_youtube_embed_url(url:str) -> bool:
+    if "https://www.youtube.com/embed/" in url:
+        return True
+    else:
+        return False 
--- tune_data/tune_data.py
+++ tune_data/tune_data.py
@@ -1,10 +1,10 @@
-from tune_data.model.tune import Tune
+from tune_data.model.tune import Tune, YoutubeVideoEmbed, AudioReferences
 
 tune_list: list[Tune] = [
-    Tune(
-        'Concertina Reel',
-        'reel',
-        """T: Concertina Reel
+  Tune(
+    'Concertina Reel',
+    'reel',
+    """T: Concertina Reel
 R: reel
 M: C|
 K: D
@@ -12,44 +12,49 @@ K: D
   A2FA BAFA|A2FA BAFE|FABc d2dB|AFEF D2FG:|
 |:Add2 Add2|AddA BAFA|B2cA BAcA|BAcA BAFG |
   Add2 Add2|AddA BAFE|FABc d2dB|AFEF D4  :|""",
-    ['https://www.youtube.com/watch?v=v0EMMbo9ikw']
-    )
+    [
+      YoutubeVideoEmbed("Foinn Seisiun 1 CD's", 'accordion, banjo, concertina, fiddle, flute', 'https://www.youtube.com/embed/j1mfZ15mPeA'),
+      YoutubeVideoEmbed("Hatao's Irish Tune of the Day #131", 'flute', 'https://www.youtube.com/embed/v0EMMbo9ikw')
+    ],
+    [
+      AudioReferences('Slow Playing of the Concertina Reel', 'concertina', '08 The Concertina (slow).mp3')
+    ]
+  ),
 #--------------------------------------------------------------    
-#     Tune('Ships are Sailing',
-#          'reel',
-#         """T:Ships are Sailing
-# R:reel
-# M:C|
-# K:Edor
-# Beed B2dB|AF~F2 dFAF|EFGA BAFB|ABdf eB~B2|
-# ~e3d B2dB|AF~F2 dFAF|EFGA Bdef|1 gefd Beed:|2 gefd Beef||
-# |:~g3a bgeg|fefg afdf|~g3a bgeg|(3fed ed Beef|
-# ~g3a bgeg|fefg afdf|g2bg f2af|1 gefd Beef:|2 gefd Beed||""",
-#     ['https://www.youtube.com/watch?v=9gLsQg38m5E']
-#     ),
-#--------------------------------------------------------------
-#     Tune('Father Kelly`s',
-#         'reel',
-#         """T:Father Kelly`s
-# R:reel
-# M:C|
-# K:G
-# B2GB AGEG|DGGF G2AB|c2AB cBAG|EAAG FDGA|
-# B2GB AGEG|DGGF GABc|d2Bd gdBd|1 cAFA ~G3A:|2 cAFA GABc||
-# |:d2Bd gdBd|d2Bd gdBd|e2ce agfe|defg agfe|
-# d2Bd gdBd|d2Bd gdBd|c2Ac BAGB|1 AGFA GABc:|2 AGFA ~G3A||)""",
-#     ['']
-#     ),
-#--------------------------------------------------------------
-#     Tune('Thrush in the Straw',
-#         'jig',
-#         """T:Thrush in the Straw
-# R:jig
-# M:6/8
-# efg edB|cBA BGE|~G3 BdB|BAG ABd|
-# efg edB|cBA BGE|~G3 BdB|1 AGF GBd:|2 AGF G2D||
-# GBd ~g3|agf g2e|fed edB|AGA BGE|
-# GBd ~g3|agf g2e|fed edB|1 AGF G2D:|2 AGF GBd||""",
-#     ['']
-#     )
+  Tune(
+    'Merry Blacksmith',
+    'reel',
+    """T: Merry Blacksmith
+R: reel
+M: C|
+K: G
+  d2dA BAFA|ABdA BAFA|ABde f2ed|Beed e2fe |
+  d2dA BAFA|ABdA BAFA|ABde f2ed|BAFE D4  :|
+|:a2ag f2fe|d2dA BAFA|ABde f2ed|BAFE efge |
+  a2ag f2fe|d2dA BAFA|ABde f2ed|BAFE D4  ||""",
+  [
+    YoutubeVideoEmbed("Foinn Seisiun 1 CD's", 'accordion, banjo, concertina, fiddle, flute', 'https://www.youtube.com/embed/dJz8TPijdXk'),
+    YoutubeVideoEmbed("Hatao's Irish Tune of the Day #151", 'flute', 'https://www.youtube.com/embed/pIQXhPhzEXE')
+  ],
+  []
+  ),
+#--------------------------------------------------------------    
+  Tune(
+    'Sally Gardens',
+    'reel',
+    """T: Sally Gardens
+R: reel
+M: C|
+K: G
+BA| G2DG B2GB|dBeB dBAB|d2Bd efge|   dBAB GEDE |
+    G2DG B2GB|dBeB dBAB|d2Bd efge|1) dBAB G2BA:|2) dBAB GABc||
+  |:dggf g2de|g2bg aged|eaag a2eg|   a2bg ageg |
+    dggf g2de|g2bg ageg|d2Bd efge|1) dBAB GABc:|2) dBAB G2BA||""",
+  [
+    YoutubeVideoEmbed("Foinn Seisiun 1 CD's", 'accordion, banjo, concertina, fiddle, flute', 'https://www.youtube.com/embed/ohQ2cF4H6NE'),
+    YoutubeVideoEmbed("Hatao's Irish Tune of the Day #11", 'flute', 'https://www.youtube.com/embed/ispQQ5GMzjc'),
+    YoutubeVideoEmbed("Banjo Buddy", "banjo", "https://www.youtube.com/embed/TuONrq0tQSo")
+  ],
+  []
+  )
 ]
